de36b9a350ade1e1d3c285e85e8305765af356e0
Joshua Oren <joshuadavidoren@gmail.com>
2021-06-28 13:26:26 -0500

    included files checked over, GET set up client and server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,32 @@
 console.log('client.js sourced');
 
-$( document ).ready( onReady );
+$(document).ready(onReady);
 
 function onReady() {
     console.log('DOM ready');
+    getArray()
+}
+// let jokes = [
+//   {
+//     whoseJoke: "Luke",
+//     jokeQuestion: "Two fish are in a tank. What did one fish say to the other?",
+//     punchLine: "Do you know how to drive this thing?"
+//   }
+// ];
+
+function getArray() {
+    $.ajax({
+            method: 'GET',
+            url: '/funny'
+        })
+        .then(function(jokes) {
+            console.log('Knock Knock, array', jokes);
+            renderMyArray(jokes);
+        })
+        .catch(function(error) {
+            console.log('can not GET', error);
+        });
+}
+renderMyArray(array) {
+    console.log(array[1]);
 }
--- server/server.js
+++ server/server.js
@@ -1,42 +1,55 @@
-const express = require( 'express' );
+const express = require('express');
 const app = express();
-const bodyParser = require( 'body-parser' );
+const bodyParser = require('body-parser');
 const PORT = 5000;
 
 // use bodyParser.urlencoded throughout the app with this:
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let jokes = [
-  {
-    whoseJoke: "Danny",
-    jokeQuestion: "Why do scuba divers fall backwards out of boats?",
-    punchLine: "If they fell forwards they’d still be in the boat!"
-  },
-  {
-    whoseJoke: "Luke",
-    jokeQuestion: "Two fish are in a tank. What did one fish say to the other?",
-    punchLine: "Do you know how to drive this thing?"
-  },
-  {
-    whoseJoke: "Millie",
-    jokeQuestion: "What do you call a pile of cats?",
-    punchLine: "A meowntain!"
-  },
-  {
-    whoseJoke: "dEv",
-    jokeQuestion: "Why should you not play cards in the forest?",
-    punchLine: "Too many Cheetahs!"
-  },
-  {
-    whoseJoke: "Scott",
-    jokeQuestion: "I went to the zoo the other day, it had one dog...",
-    punchLine: "It was a shih tzu."
-  }
+let jokes = [{
+        whoseJoke: "Danny",
+        jokeQuestion: "Why do scuba divers fall backwards out of boats?",
+        punchLine: "If they fell forwards they’d still be in the boat!"
+    },
+    {
+        whoseJoke: "Luke",
+        jokeQuestion: "Two fish are in a tank. What did one fish say to the other?",
+        punchLine: "Do you know how to drive this thing?"
+    },
+    {
+        whoseJoke: "Millie",
+        jokeQuestion: "What do you call a pile of cats?",
+        punchLine: "A meowntain!"
+    },
+    {
+        whoseJoke: "dEv",
+        jokeQuestion: "Why should you not play cards in the forest?",
+        punchLine: "Too many Cheetahs!"
+    },
+    {
+        whoseJoke: "Scott",
+        jokeQuestion: "I went to the zoo the other day, it had one dog...",
+        punchLine: "It was a shih tzu."
+    }
 ];
 
 // serve back static files
 app.use(express.static('server/public'));
 
 app.listen(PORT, () => {
-  console.log('server running on: ', PORT);
+    console.log('I\'m the port I\'m the port I\'m the port I\'m the port, I am the port', PORT);
 }); // end spin up server
+
+// let jokes = [
+//   {
+//     whoseJoke: "",
+//     jokeQuestion: "",
+//     punchLine: ""
+//   }
+// ];
+
+app.get('/funny', function(req, res) {
+    console.log('Request method: ', req.method);
+    console.log('Request for /funny was made');
+    res.send(jokes);
+});
